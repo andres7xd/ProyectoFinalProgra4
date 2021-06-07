f3dd9b3cec10097be41158e5c720a06fb8b289ba
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2021-06-07 16:43:44 -0600

    Un poco de css xs

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,14 +3,15 @@
   <div id="panel_app">
 
     <div id="user_box">
-      <a href="<?php echo site_url('user/edit/' . $this->session->userdata['logged_in']['usuario_id']); ?>" title="Editar Perfil">
+      <!-- <a href="<?php echo site_url('user/edit/' . $this->session->userdata['logged_in']['usuario_id']); ?>" title="Editar Perfil">
         <?php
         echo "<img src='" . site_url('/resources/photos/' . $this->session->userdata['logged_in']['foto'])
           . "' alt='photo_profile' width=50 height=50 id='photo_profile' />" .
           "<span>HOLA! " . $this->session->userdata['logged_in']['nombre_real'] . ". ✎</span>";
-          
         ?>
-      </a>
+      </a> -->
+
+      <img id="icono_marketplace" src="<?php echo site_url("/resources/icons/marketplace.png") ?>" width=200 height=200>
 
       <div id="logout">
         <?php echo form_open('auth/logout'); ?>
@@ -20,89 +21,74 @@
 
     </div>
 
-
-
-    <div id="post_box">
+    <!-- <div id="post_box">
 
       <?php echo form_open_multipart('twitter/process', 'onsubmit="send()"'); ?>
       <br>
 
-      <!-- <button type="submit" id="btn_save" name="btn_save" value="btn_save" class="boton" tabindex="2" title="Publicar">Publicar</button> -->
+      <input type="text" class="cajatexto" id="txt_nombre" value="" placeholder="Escribe aquí para buscar!">
       <button type="submit" name="btn_search" id="btn_search" value="btn_search" class="boton" title="Buscar">🔍</button>
 
       <span style="color: #f00"><?php echo form_error('txt_post'); ?></span>
       <?php echo form_close(); ?>
 
-      <h1>Tiendas</h1>
 
-
-
-    </div>
+    </div> -->
 
     <br>
   </div>
 
   <div>
-
-  
-
-
     <div id="main_panel">
-
-    <h1 align="center">LISTA DE TIENDAS</h1>
-    <table width="70%" border="1px" align="center">
-
-    <tr align="center">
-        
-        <td>Icono</td>
-        <td>Nombre</td>
-        <td>Perfil</td>
-       
-    </tr>
-
-      <?php foreach ($usuarios_empresa as $u) { ?>
-      
-
-        <!-- <div class='post_block'>
-          <span class='post_text' id='post_<?php echo $u['usuario_id']; ?>'>
-          </span>
-          <div id='content_post_<?php echo $u['usuario_id']; ?>'> -->
-
-          
-            <tr>
-                
-                <td width="150"><?php echo "<img src='" . site_url('/resources/photos/' . $u['foto'])
-                . "' alt=' Foto' title=' Foto'  width=150 height=100 id='foto_file' />"; ?></td>
-                <td align="center"><?php echo $u["nombre_real"]?></td>
-                <td>
-                <input type="button">
-                </td>
-               
-            </tr>
-        
-
-
-          <!-- </div> -->
-
-         
-
-
-
-
-
-
-
-
-
+      <h1 align="center" style="text-decoration: underline; font-family: Century Gothic">LISTA DE TIENDAS</h1>
+      <table width="50%" border="1px" align="center">
+        <thead>
+          <tr>
+            <th>ÍCONO</th>
+            <th>NOMBRE</th>
+            <th>PERFIL</th>
+          </tr>
+        </thead>
+        <?php foreach ($usuarios_empresa as $u) { ?>
+          <tr>
+            <td align="center" width="150"><?php echo "<img src='" . site_url('/resources/photos/' . $u['foto'])
+                              . "' alt=' Foto' title=' Foto'  width=50 height=50 id='foto_file' />"; ?></td>
+            <td align="center" width="150" style="font-family: Century Gothic; font-weight: bold; font-size:15px;"><?php echo $u["nombre_real"] ?></td>
+            <td align="center">
+              <input type="button" class="btnPerfil" value="Ver Perfil">
+            </td>
+          </tr>
         <?php } ?>
-
-
-
-        </div>
+        </table>
     </div>
 
-  <?php
+    <div id="main_panel">
+      <h1 align="center" style="text-decoration: underline; font-family: Century Gothic">PRODUCTOS MÁS VENDIDOS</h1>
+      <table width="50%" border="1px" align="center">
+        <thead>
+          <tr>
+            <th>ÍCONO</th>
+            <th>NOMBRE</th>
+            <th>PERFIL</th>
+          </tr>
+        </thead>
+        <?php foreach ($usuarios_empresa as $u) { ?>
+          <tr>
+            <td align="center" width="150"><?php echo "<img src='" . site_url('/resources/photos/' . $u['foto'])
+                              . "' alt=' Foto' title=' Foto'  width=50 height=50 id='foto_file' />"; ?></td>
+            <td align="center" width="150" style="font-family: Century Gothic; font-weight: bold; font-size:15px;"><?php echo $u["nombre_real"] ?></td>
+            <td align="center">
+              <input type="button" class="btnPerfil" value="Ver Perfil">
+            </td>
+          </tr>
+        <?php } ?>
+        </table>
+    </div>
+  </div>
+  
+
+<?php
 } else {
   header("location: " . base_url()); //dirección de arranque especificada en config.php y luego en routes.php
 }
-  ?>
+?>
