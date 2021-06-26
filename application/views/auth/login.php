cc31292088bd62516adf2371b85f60553f867e96
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2021-06-26 05:42:22 -0600

    último commit, ahora sí

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,12 +5,11 @@
 	<meta charset="utf-8">
 	<html lang="es" xml:lang="es" xmlns="http://www.w3.org/1999/xhtml">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Chatwitter</title>
+	<title>Marketplace</title>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<link rel="icon" href="<?php echo site_url('resources/img/favicon.png'); ?>" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href="<?php echo site_url('resources/css/login.css'); ?>">
-
 
 </head>
 
